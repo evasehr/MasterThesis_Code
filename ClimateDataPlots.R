a700e5c9a1ba52a6e5c03772cb5564f0700aa7c5
Eva Sehr <evamaria.sehr@gmail.com>
2020-11-20 11:26:26 +0100

    RF models added

--- ClimateDataPlots.R
+++ ClimateDataPlots.R
@@ -18,14 +18,15 @@ climate <- getData('worldclim', var='bio', res=2.5)
 #define color palette for plots
 tempcol <- colorRampPalette(c("purple", "blue", "skyblue", "green", "lightgreen", "yellow", "orange", "red", "darkred"))
 tempcol2 <- colorRampPalette(c("#f7fbff","#4292c6","#2171b5","#08519c","#08306b"))
+tempcol3 <- colorRampPalette(c("#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f", "#990000"))
 
 #BIO1 = Annual Mean Temperature
 par(mar=c(5,5,3,3), bty="n")
-plot(climate$bio1, legend = FALSE, col=tempcol(100), xlim = c(-10, 40), ylim = c(35, 70), legend.mar=20, xlab = "Longitude", ylab="Latitude")
+plot(climate$bio1, legend = FALSE, col=tempcol3(100), xlim = c(-10, 40), ylim = c(35, 70), legend.mar=20, xlab = "Longitude", ylab="Latitude")
 points(GPS_coord_7locs$...2,GPS_coord_7locs$...3, pch=16 )
 text(GPS_coord_7locs$...2+1, GPS_coord_7locs$...3+1, GPS_coord_7locs$...4)
 title("Annual Mean Temperature", line = 1)
-plot(climate$bio1, legend.only=TRUE, col=tempcol(100),
+plot(climate$bio1, legend.only=TRUE, col=tempcol3(100),
      legend.width=0.7, legend.shrink=0.5,
      legend.args=list(text='Annual Mean Temperature [°C *10]', side=4, font=2, line=3, cex=0.9))
 
